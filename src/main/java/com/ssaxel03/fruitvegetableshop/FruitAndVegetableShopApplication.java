10dbb69f03b44d97de015e0b60a514a2c6ecd121
axel <ssaxel03@gmail.com>
2025-03-13 21:21:53 +0000

    BATMAN

---
+++ src/main/java/com/ssaxel03/fruitvegetableshop/FruitAndVegetableShopApplication.java
@@ -0,0 +1,13 @@
+package com.ssaxel03.fruitvegetableshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitAndVegetableShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitAndVegetableShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssaxel03/fruitvegetableshop/FruitAndVegetableShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssaxel03.fruitvegetableshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitAndVegetableShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
